ef3b87b7cef0ec76b48520a771b77cbe52f3769e
Alex McHale <alexmchale@gmail.com>
2011-05-17 09:37:44 -0500

    Add support for Rails3's ActionDispatch::Request.

--- lib/api_auth/headers.rb
+++ lib/api_auth/headers.rb
@@ -19,6 +19,8 @@ module ApiAuth
         @request = ActionControllerRequest.new(request)
       when /ActionController::TestRequest/
         @request = ActionControllerRequest.new(request)
+      when /ActionDispatch::Request/
+        @request = ActionControllerRequest.new(request)
       else
         raise UnknownHTTPRequest, "#{request.class.to_s} is not yet supported."
       end
