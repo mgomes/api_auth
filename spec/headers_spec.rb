9534bafd7c246ee41ad4e47b363f5c4856a7ca81
Kevin Glowacz <kevin@glowacz.info>
2012-08-31 15:47:42 -0500

    remove whitespace

--- spec/headers_spec.rb
+++ spec/headers_spec.rb
@@ -1,73 +1,73 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "ApiAuth::Headers" do
-  
+
   CANONICAL_STRING = "text/plain,e59ff97941044f85df5297e1c302d260,/resource.xml?foo=bar&bar=foo,Mon, 23 Jan 1984 03:29:56 GMT"
 
   describe "with Net::HTTP" do
-  
+
     before(:each) do
-      @request = Net::HTTP::Put.new("/resource.xml?foo=bar&bar=foo", 
-        'content-type' => 'text/plain', 
+      @request = Net::HTTP::Put.new("/resource.xml?foo=bar&bar=foo",
+        'content-type' => 'text/plain',
         'content-md5' => 'e59ff97941044f85df5297e1c302d260',
         'date' => "Mon, 23 Jan 1984 03:29:56 GMT")
       @headers = ApiAuth::Headers.new(@request)
     end
-    
+
     it "should generate the proper canonical string" do
       @headers.canonical_string.should == CANONICAL_STRING
     end
-    
+
     it "should set the authorization header" do
       @headers.sign_header("alpha")
       @headers.authorization_header.should == "alpha"
     end
-    
+
     it "should set the DATE header if one is not already present" do
-      @request = Net::HTTP::Put.new("/resource.xml?foo=bar&bar=foo", 
-        'content-type' => 'text/plain', 
+      @request = Net::HTTP::Put.new("/resource.xml?foo=bar&bar=foo",
+        'content-type' => 'text/plain',
         'content-md5' => 'e59ff97941044f85df5297e1c302d260')
       ApiAuth.sign!(@request, "some access id", "some secret key")
       @request['DATE'].should_not be_nil
     end
-  
+
   end
-  
+
   describe "with RestClient" do
-  
+
     before(:each) do
       headers = { 'Content-MD5' => "e59ff97941044f85df5297e1c302d260",
                   'Content-Type' => "text/plain",
                   'Date' => "Mon, 23 Jan 1984 03:29:56 GMT" }
-      @request = RestClient::Request.new(:url => "/resource.xml?foo=bar&bar=foo", 
+      @request = RestClient::Request.new(:url => "/resource.xml?foo=bar&bar=foo",
         :headers => headers,
         :method => :put)
       @headers = ApiAuth::Headers.new(@request)
     end
-    
+
     it "should generate the proper canonical string" do
       @headers.canonical_string.should == CANONICAL_STRING
     end
-    
+
     it "should set the authorization header" do
       @headers.sign_header("alpha")
       @headers.authorization_header.should == "alpha"
     end
-    
+
     it "should set the DATE header if one is not already present" do
       headers = { 'Content-MD5' => "e59ff97941044f85df5297e1c302d260",
                   'Content-Type' => "text/plain" }
-      @request = RestClient::Request.new(:url => "/resource.xml?foo=bar&bar=foo", 
+      @request = RestClient::Request.new(:url => "/resource.xml?foo=bar&bar=foo",
         :headers => headers,
         :method => :put)
       ApiAuth.sign!(@request, "some access id", "some secret key")
       @request.headers['DATE'].should_not be_nil
     end
-  
+
   end
-  
+
   describe "with Curb" do
-  
+
     before(:each) do
       headers = { 'Content-MD5' => "e59ff97941044f85df5297e1c302d260",
                   'Content-Type' => "text/plain",
@@ -77,16 +77,16 @@ describe "ApiAuth::Headers" do
       end
       @headers = ApiAuth::Headers.new(@request)
     end
-    
+
     it "should generate the proper canonical string" do
       @headers.canonical_string.should == CANONICAL_STRING
     end
-    
+
     it "should set the authorization header" do
       @headers.sign_header("alpha")
       @headers.authorization_header.should == "alpha"
     end
-    
+
     it "should set the DATE header if one is not already present" do
       headers = { 'Content-MD5' => "e59ff97941044f85df5297e1c302d260",
                   'Content-Type' => "text/plain" }
@@ -96,9 +96,9 @@ describe "ApiAuth::Headers" do
       ApiAuth.sign!(@request, "some access id", "some secret key")
       @request.headers['DATE'].should_not be_nil
     end
-  
+
   end
-  
+
   describe "with ActionController" do
 
     before(:each) do
