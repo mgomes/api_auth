d0926133521b1b029db223b94dd583106740d14d
Benjamin Seibert <benjamin.seibert@roomthirteen.de>
2013-11-13 17:11:55 +0100

    added support for sinatra requests

--- lib/api_auth/headers.rb
+++ lib/api_auth/headers.rb
@@ -27,6 +27,8 @@ module ApiAuth
         @request = ActionDispatchRequest.new(request)
       when /Rack::Request/
         @request = RackRequest.new(request)
+      when /Sinatra::Request/
+          @request = RackRequest.new(request)
       else
         raise UnknownHTTPRequest, "#{request.class.to_s} is not yet supported."
       end
