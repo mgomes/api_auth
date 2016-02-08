6b90f2f17dc03d29bf6c7f6bf402b55bfb095087
Lee Henson <lee.m.henson@gmail.com>
2016-02-08 14:36:18 +0000

    require rails before apiauth in tests so railtie can hook into existing classes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,6 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
-require 'api_auth'
-require 'amatch'
-require 'rest_client'
-require 'curb'
-require 'httpi'
-require 'faraday'
-require 'net/http/post/multipart'
 
 require 'active_support'
 require 'active_support/test_case'
@@ -15,6 +8,14 @@ require 'action_controller'
 require 'action_controller/test_case'
 require 'active_resource'
 require 'active_resource/http_mock'
+
+require 'api_auth'
+require 'amatch'
+require 'rest_client'
+require 'curb'
+require 'httpi'
+require 'faraday'
+require 'net/http/post/multipart'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
