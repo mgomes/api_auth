79176e34e1e6b957609dd59c37d5bce780314a01
Aidan Coyle <packrat386@gmail.com>
2016-07-27 14:28:54 -0500

    Fall back to #fullpath
    Some versions of Rails (notably 3.0) don't have #originl_fullpath, so we
    have to fall back to #fullpath.

--- lib/api_auth/request_drivers/action_dispatch.rb
+++ lib/api_auth/request_drivers/action_dispatch.rb
@@ -2,7 +2,11 @@ module ApiAuth
   module RequestDrivers # :nodoc:
     class ActionDispatchRequest < ActionControllerRequest # :nodoc:
       def request_uri
-        @request.original_fullpath
+        if @request.respond_to?(:original_fullpath)
+          @request.original_fullpath
+        else
+          @request.fullpath
+        end
       end
     end
   end
