9b2a0ce1ae16a3e2cb34606c1959ea9fecf3dd72
Taylor Thurlow <taylorthurlow@me.com>
2020-05-05 05:48:19 -0700

    Fix Rubocop warnings after recent changes to Rubocop (#180)
    * Update changed rubocop cop name
    
    * Fix code to adhere to new Rubocop cops
    
    The cops in question are:
    - Lint/SendWithMixinArgument
    - Lint/NonDeterministicRequireOrder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,4 +21,4 @@ require 'net/http/post/multipart'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
